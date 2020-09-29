665ede6c5b4f60a2d6328157cc31f331ee106989
Rijoanul Hasan <rijoanul.shanto@gmail.com>
2020-09-29 22:31:49 +0600

    all configuration tested and updated

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+const prefix = "?";
+
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "hello") {
+    message.reply("Hi!");
+  }
+});
+
 client.login(config.BOT_TOKEN);
 
 // console.log(config.BOT_TOKEN);
