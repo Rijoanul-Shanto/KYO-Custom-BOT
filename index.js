51775e5e65e37f3375036b7ca0597a2b8625eba3
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2020-10-07 20:30:44 +0600

    server status added

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ client.on("message", function (message) {
     message.reply("Hello!");
   } else if (command == "avatar") {
     message.reply(message.author.displayAvatarURL());
+  } else if (command == "server") {
+    client.guilds.cache.forEach((guild) => {
+      // console.log(guild.emojis);
+      const embed = new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle(guild.name)
+        .addField("Total Members:", guild.memberCount)
+        .addField("Server Created On:", guild.createdAt)
+        .addField("Verification Level", guild.verificationLevel)
+        .setThumbnail(
+          "https://cdn.discordapp.com/icons/505848168668332052/d6fe436dc8e4b175e3884e5a42959327"
+        )
+        .addFields(
+          { name: "Server Owner:", value: guild.owner, inline: true },
+          {
+            name: "Server Superuser:",
+            value: client.users.cache.get("498513765608849408"),
+            inline: true,
+          }
+        )
+        .setFooter(
+          message.author.username + " requested server info",
+          message.author.avatarURL()
+        )
+        .setTimestamp();
+      message.channel.send(embed);
+    });
   }
 });
 
